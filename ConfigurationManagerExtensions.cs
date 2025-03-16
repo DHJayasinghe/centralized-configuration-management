1d9c937db041db7ce19df9e8b1d44f6e9b850f73
Dhanuka Jayasinghe <hasitha2kandy@gmail.com>
2025-03-16 14:43:21 +0000

    Bit of refactoring

---
+++ ConfigurationManagerExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsulConfigurationManagement;
+
+public static class ConfigurationManagerExtensions
+{
+    public static ConfigurationManager AddConsulConfiguration(
+        this ConfigurationManager manager,
+        bool reloadOnChange = false, TimeSpan? changeChecKInterval = null, string versionFlag = "Version")
+    {
+        var consulEndpoint = manager.GetConnectionString("Consul");
+        var consulConfiguration = new ConsulConfiguration(consulEndpoint, reloadOnChange, changeChecKInterval, versionFlag);
+
+        IConfigurationBuilder configBuilder = manager;
+        configBuilder.Add(new ConsulConfigurationSource(consulConfiguration));
+
+        return manager;
+    }
+}
---
+++ ConsulConfigurationSource.cs
@@ -0,0 +1,7 @@
+﻿namespace ConsulConfigurationManagement;
+
+public class ConsulConfigurationSource(ConsulConfiguration configuration) : IConfigurationSource
+{
+    private readonly ConsulConfiguration _configuration = configuration;
+    public IConfigurationProvider Build(IConfigurationBuilder builder) => new ConsulConfigurationProvider(_configuration);
+}
